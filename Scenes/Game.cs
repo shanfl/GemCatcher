6defd036223dabcb87c4294bd863f1969266c043
shanfl <shanfulin@baiyyy.com.cn>
2025-12-10 12:59:25 +0800

    3-method to get node

--- Scenes/Game.cs
+++ Scenes/Game.cs
@@ -5,14 +5,25 @@ public partial class Game : Node2D
 {
 
 	[Export] private Gem _gem;
+	[Export] private NodePath _gemPath;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("Game Scene Loaded");
 
+		// method 1: using exported NodePath
 		//Gem gem = GetNode<Gem>("Gem");
 		//gem.OnScored += OnGemScored;
-		_gem.OnScored += OnGemScored;
+		
+		//method 2: using exported Gem variable		
+		//_gem.OnScored += OnGemScored;
+
+		//method 3: using GetNode with the NodePath
+		Gem gem = GetNode<Gem>(_gemPath);
+		gem.OnScored += OnGemScored;
+
+		//
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
